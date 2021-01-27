a51ecc56b09beb865f56ccc142105319c5e85d02
zeeshan <itexpert120@gmail.com>
2021-01-27 20:26:09 +0500

    new lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,8 @@
 LOCAL_PATH := device/samsung/a3ulte
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/rr_a3ulte.mk \
-	$(LOCAL_DIR)/pa_a3ulte.mk \
 	$(LOCAL_DIR)/lineage_a3ulte.mk
+	
+COMMON_LUNCH_CHOICES := \
+    lineage_a3ulte-eng \
+    lineage_a3ulte-userdebug \
+    lineage_a3ulte-user
